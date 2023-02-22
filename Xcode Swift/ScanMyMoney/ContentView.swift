bce51d6e8a6bdaf4110d5dd58ce76174b8fe530a
Chrislucky7 <cfabian2@ucmerced.edu>
2023-02-22 12:57:46 -0800

    Xcode start

---
+++ Xcode Swift/ScanMyMoney/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ScanMyMoney
+//
+//  Created by Christopher Fabian on 1/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode Swift/ScanMyMoney/ScanMyMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanMyMoneyApp.swift
+//  ScanMyMoney
+//
+//  Created by Christopher Fabian on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ScanMyMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
