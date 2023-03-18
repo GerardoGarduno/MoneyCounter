76ab68d078916d6765a7613ee53efa0b378bd372
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2023-03-17 18:58:47 -0700

    the model is only called when the route is being accessed.(FIXED)

--- backend/roboflowTest.py
+++ backend/roboflowTest.py
@@ -17,36 +17,36 @@ image_folder = os.path.join(script_folder, 'images')
 #save the iles
 image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith('.jpg')]
 
-# Make predictions for each image in the folder
-total = 0
-bills = []
-for image_file in image_files:
-    prediction = model.predict(image_file, confidence=40, overlap=30).json()
-    for obj in prediction['predictions']:
-        label = obj['class']
-        confidence = obj['confidence']
-        bills.append({'label': label, 'confidence': confidence})
-        if label == '100Dollar':
-            total += 100
-        elif label == '50Dollar':
-            total += 50
-        elif label == '20Dollar':
-            total += 20
-        elif label == '10Dollar':
-            total += 10
-        elif label == '5Dollar':
-            total += 5
-        elif label == '1Dollar':
-            total += 1
-    #print(prediction, "\n")
-print("Total amount of money in the images folder:", total)
-
 @app.route('/total')
 def get_total():
+    total = 0
+    for image_file in image_files:
+        prediction = model.predict(image_file, confidence=40, overlap=30).json()
+        for obj in prediction['predictions']:
+            label = obj['class']
+            if label == '100Dollar':
+                total += 100
+            elif label == '50Dollar':
+                total += 50
+            elif label == '20Dollar':
+                total += 20
+            elif label == '10Dollar':
+                total += 10
+            elif label == '5Dollar':
+                total += 5
+            elif label == '1Dollar':
+                total += 1
     return jsonify({'total': total})
 
 @app.route('/bills')
 def get_bills():
+    bills = []
+    for image_file in image_files:
+        prediction = model.predict(image_file, confidence=40, overlap=30).json()
+        for obj in prediction['predictions']:
+            label = obj['class']
+            confidence = obj['confidence']
+            bills.append({'label': label, 'confidence': confidence})
     return jsonify({'bills': bills})
 
 if __name__ == '__main__':
