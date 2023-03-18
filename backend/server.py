2d6a13d8fa91c6ab5dd6e0ebe3ed25dce2145ff2
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2023-03-18 01:00:27 -0700

    upload button/ renders new value after upload
    TODO: only run the model on the new uploaded images dont run it for the entire folder again.

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,29 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from roboflow import Roboflow
 import os
-from flask import Flask, jsonify
 
 app = Flask(__name__)
+CORS(app)
 
-#establising connection to ROBOFLOW api
+# Establishing connection to ROBOFLOW api
 rf = Roboflow(api_key="sqDcYOoGebzrxPm0n9sO")
-
 project = rf.workspace().project("usd-money")
 model = project.version(2).model
 
-#find the image folder relative to this python  scipt
+# Find the image folder relative to this python script
 script_folder = os.path.dirname(os.path.abspath(__file__))
 image_folder = os.path.join(script_folder, 'images')
 
-#save the iles
-image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith('.jpg')]
+# Initialize total and bills
+total = 0
+bills = []
 
-@app.route('/total')
-def get_total():
+def process_images():
+    global total, bills
     total = 0
+    bills = []
+    image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith('.jpg')]
     for image_file in image_files:
         prediction = model.predict(image_file, confidence=40, overlap=30).json()
         for obj in prediction['predictions']:
@@ -36,18 +40,33 @@ def get_total():
                 total += 5
             elif label == '1Dollar':
                 total += 1
+            confidence = obj['confidence']
+            bills.append({'label': label, 'confidence': confidence})
+
+# Process the images once at server startup
+process_images()
+
+@app.route('/total')
+def get_total():
     return jsonify({'total': total})
 
 @app.route('/bills')
 def get_bills():
-    bills = []
-    for image_file in image_files:
-        prediction = model.predict(image_file, confidence=40, overlap=30).json()
-        for obj in prediction['predictions']:
-            label = obj['class']
-            confidence = obj['confidence']
-            bills.append({'label': label, 'confidence': confidence})
     return jsonify({'bills': bills})
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        files = request.files.getlist('file')
+        for file in files:
+            file.save(os.path.join(app.root_path, 'images', file.filename))
+        process_images()  # Process the uploaded images to update total and bills
+        return jsonify({'success': True})
+    except Exception as e:
+        print(str(e))
+        return jsonify({'success': False}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ frontendReact/src/App.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bills: [],
+      total: null,
+      uploading: false
+    };
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    fetch('http://localhost:5000/total')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ total: data.total });
+      });
+  
+    fetch('http://localhost:5000/bills')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ bills: data.bills });
+      });
+  }
+
+  // handleUpload(event) {
+  //   console.log("eneterd");
+  //   event.preventDefault();
+  //   const formData = new FormData();
+  //   const fileInput = document.querySelector('#file');
+    
+  //   if (!fileInput) {
+  //     console.error('File input element not found');
+  //     return;
+  //   }
+    
+  //   const files = fileInput.files;
+
+  //   for (let i = 0; i < files.length; i++) {
+  //     formData.append('file', files[i]);
+  //   }
+
+  //   this.setState({ uploading: true });
+  //   fetch('http://localhost:5000/upload', {
+  //     method: 'POST',
+  //     body: formData
+  //   }).then(response => {
+  //     if (response.ok) {
+  //       alert('Upload successful');
+  //       fileInput.value = null; // reset the file input field
+  //       this.setState({ uploading: false });
+  //     } else {
+  //       alert('Upload failed');
+  //       this.setState({ uploading: false });
+  //     }
+  //   });
+  // }
+  handleUpload(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    const fileInput = document.querySelector('#file');
+      
+    if (!fileInput) {
+      console.error('File input element not found');
+      return;
+    }
+      
+    const files = fileInput.files;
+  
+    for (let i = 0; i < files.length; i++) {
+      formData.append('file', files[i]);
+    }
+  
+    this.setState({ uploading: true });
+    fetch('http://localhost:5000/upload', {
+      method: 'POST',
+      body: formData
+    }).then(response => {
+      if (response.ok) {
+        alert('Upload successful');
+        fileInput.value = null; // reset the file input field
+        
+        // fetch updated data after successful upload
+        this.getData();
+        
+        this.setState({ uploading: false });
+      } else {
+        alert('Upload failed');
+        this.setState({ uploading: false });
+      }
+    });
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Total Amount of Money:</h1>
+        <h2>{this.state.total}</h2>
+        <h1>Bills:</h1>
+        <ul>
+          {this.state.bills.map((bill, index) => (
+            <li key={index}>
+              {bill.label} - Confidence: {bill.confidence}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleUpload}>
+          <input id="file" type="file" name="file" multiple />
+          <button type="submit" disabled={this.state.uploading}>Upload</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default App;
