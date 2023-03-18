d1b48f388cec7e36b0d5e3d1e2984992a3a6e4fb
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2023-03-17 18:50:21 -0700

    TWO JSON files one that shows the bills with condifence level and the other JSON ffile shows the total amount
    bug ?: model is being called twice?  once when it starts the py script and again when its asked to count the bills ?

--- backend/roboflowTest.py
+++ backend/roboflowTest.py
@@ -1,13 +1,14 @@
 from roboflow import Roboflow
 import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
 #establising connection to ROBOFLOW api
 rf = Roboflow(api_key="sqDcYOoGebzrxPm0n9sO")
 
 project = rf.workspace().project("usd-money")
 model = project.version(2).model
-
-# infer on a local image
-#print(model.predict("1dollarexample.jpg", confidence=40, overlap=30).json())
 
 #find the image folder relative to this python  scipt
 script_folder = os.path.dirname(os.path.abspath(__file__))
@@ -17,32 +18,36 @@ image_folder = os.path.join(script_folder, 'images')
 image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith('.jpg')]
 
 # Make predictions for each image in the folder
-total = 0;
+total = 0
+bills = []
 for image_file in image_files:
     prediction = model.predict(image_file, confidence=40, overlap=30).json()
     for obj in prediction['predictions']:
         label = obj['class']
         confidence = obj['confidence']
-        print(f"Image: {image_file} | Label: {label} | Confidence: {confidence}")
-        if (label == '100Dollar'):
+        bills.append({'label': label, 'confidence': confidence})
+        if label == '100Dollar':
             total += 100
-        if (label == '50Dollar'):
+        elif label == '50Dollar':
             total += 50
-        if (label == '20Dollar'):
+        elif label == '20Dollar':
             total += 20
-        if (label == '10Dollar'):
+        elif label == '10Dollar':
             total += 10
-        if(label == '5Dollar'):
-            total +=5
-        if (label == '1Dollar'):
+        elif label == '5Dollar':
+            total += 5
+        elif label == '1Dollar':
             total += 1
     #print(prediction, "\n")
-print("Total amount of money in the images folder..:",total)
-   
+print("Total amount of money in the images folder:", total)
 
+@app.route('/total')
+def get_total():
+    return jsonify({'total': total})
 
-# visualize your prediction
-#model.predict("1dollarexample.jpg", confidence=40, overlap=30).save("prediction.jpg")
+@app.route('/bills')
+def get_bills():
+    return jsonify({'bills': bills})
 
-# infer on an image hosted elsewhere
-# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+if __name__ == '__main__':
+    app.run(debug=True)
