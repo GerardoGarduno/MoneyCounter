5d314078abe1cb40b52c466514dce52d8bcdf6e7
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2023-04-19 13:33:58 -0700

    centerd the gorup

--- frontendReact/src/App.jsx
+++ frontendReact/src/App.jsx
@@ -9,9 +9,13 @@ function App() {
   return (
     <div className="App">
       <div className="NavBar">MoneyCounter</div>
-      <div className="chooseOption"><h3>Choose Option</h3> </div>
-      <button className="groupofButton1">Camera</button>
-      <button className="groupofButton2" >Virtual Wallet</button>
+      <div className="centerBlock">
+        test
+        <div className="chooseOption"><h3>Choose Option</h3> </div>
+        <button className="groupofButton1">Camera</button>
+        <button className="groupofButton2" >Virtual Wallet</button>
+      </div>
+    
    
     </div>
   )
