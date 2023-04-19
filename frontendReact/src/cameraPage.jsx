9c24c207e7d96998d4bf41afeb11e38ca6d875eb
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2023-04-19 13:04:57 -0700

    hopegame for webb version
    need to center the rest of the block, link the bbuttons use figme for css.

---
+++ frontendReact/src/cameraPage.jsx
@@ -0,0 +1,110 @@
+import React, { Component, Link } from 'react';
+import Webcam from "react-webcam";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bills: [],
+      total: null,
+      uploading: false
+    };
+    this.capture = this.capture.bind(this); // bind capture method to component instance
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    fetch('http://localhost:5000/total')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ total: data.total });
+      });
+  
+    fetch('http://localhost:5000/bills')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ bills: data.bills });
+      });
+  }
+
+  handleUpload(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    const fileInput = document.querySelector('#file');
+      
+    if (!fileInput) {
+      console.error('File input element not found');
+      return;
+    }
+      
+    const files = fileInput.files;
+  
+    for (let i = 0; i < files.length; i++) {
+      formData.append('file', files[i]);
+    }
+  
+    this.setState({ uploading: true });
+    fetch('http://localhost:5000/upload', {
+      method: 'POST',
+      body: formData
+    }).then(response => {
+      if (response.ok) {
+        alert('Upload successful');
+        fileInput.value = null; // reset the file input field
+        
+        // fetch updated data after successful upload
+        this.getData();
+        
+        this.setState({ uploading: false });
+      } else {
+        alert('Upload failed');
+        this.setState({ uploading: false });
+      }
+    });
+  }
+  capture() {
+    const imageSrc = this.webcamRef.getScreenshot();
+    //formData.append('file', files[i]);
+
+    //need to send this to folder
+    //this.handleUpload(imageSrc);
+    console.log(imageSrc);
+  }
+  
+
+render() {
+    return (
+      <div>
+        <h1>Total Amount of Money:</h1>
+        <h2>{this.state.total}</h2>
+        <h1>Bills:</h1>
+        <ul>
+          {this.state.bills.map((bill, index) => (
+            <li key={index}>
+              {bill.label} - Confidence: {bill.confidence}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleUpload}>
+          <input id="file" type="file" name="file" multiple />
+          <button type="submit" disabled={this.state.uploading}>Upload</button>
+        </form>
+        {/* <Webcam
+          audio={false}
+          ref={ref => this.webcamRef = ref}
+          screenshotFormat="image/jpeg"
+         /> */}
+        <button onClick={this.capture}>Capture</button> 
+        {/* uncomment this line */}
+        {/* <button onClick={capture}>Capture</button>   */}
+   
+      </div>
+    );
+  }
+}
+export default App;
