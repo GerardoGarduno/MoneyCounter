df633f46acc63d60715c909ebfb6ba9323a241c4
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2023-04-25 15:43:31 -0700

    need to make the route in the backenf
    right now the post is working in the front end, but i need a route in teh be to recive the image,

--- frontendReact/src/webPages/cameraPage.jsx
+++ frontendReact/src/webPages/cameraPage.jsx
@@ -6,53 +6,71 @@ import "./webPages.css";
 function cameraPage(){
   const webcamRef = useRef(null);
   const [image,setImage] = useState(null);
+  const [screenshot,setScreenShot] = useState(false); //possible bug multiple attendpt to sedn 
   const [uploading, setUploading] = useState(false);
-    function handleUpload(event) {
-        event.preventDefault();
-        const formData = new FormData();
-        const fileInput = document.querySelector('#file');
-    
-        if (!fileInput) {
-          console.error('File input element not found');
-          return;
+  function handleUpload(event) {
+      event.preventDefault();
+      const formData = new FormData();
+      const fileInput = document.querySelector('#file');
+      if (!fileInput) {
+        console.error('File input element not found');
+        return;
+      }
+      const files = fileInput.files;
+      for (let i = 0; i < files.length; i++) {
+        formData.append('file', files[i]);
+      }
+      setUploading(true);
+      fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData
+      }).then(response => {
+        if (response.ok) {
+          alert('Upload successful');
+          fileInput.value = null; // reset the file input field
+          //block the the upload data button while proccessing data
+          setUploading(false);
+        } else {
+          alert('Upload failed');
+          setUploading(false);
         }
-    
-        const files = fileInput.files;
-    
-        for (let i = 0; i < files.length; i++) {
-          formData.append('file', files[i]);
-        }
-    
-        setUploading(true);
-        fetch('http://localhost:5000/upload', {
-          method: 'POST',
-          body: formData
-        }).then(response => {
-          if (response.ok) {
-            alert('Upload successful');
-            fileInput.value = null; // reset the file input field
-            //block the the upload data button while proccessing data
-            setUploading(false);
-          } else {
-            alert('Upload failed');
-            setUploading(false);
-          }
-        });
+      });
+  }
+  function sendimage(event) {
+    event.preventDefault();
+    if (image === null) {
+      console.error('No image captured');
+      return;
     }
-    const capture = useCallback(() =>{
-      console.log("trying to camptr")
-      const imageSrc = webcamRef.current.getScreenshot();
+    const formData = new FormData();
+    const blob = new Blob([image], {type: 'image/jpeg'});
+    formData.append('image', blob, 'image.jpg');
+    fetch('http://localhost:5000/upload_image', {
+      method: 'POST',
+      body: formData
+    }).then(response => {
+      if (response.ok) {
+        alert('Image uploaded successfully');
+      } else {
+        alert('Image upload failed');
+      }
+    }).catch(error => {
+      console.error('Error uploading image:', error);
+    });
+  }
+  
+  const capture = useCallback(() =>{
+    console.log("trying to camptr")
+    const imageSrc = webcamRef.current.getScreenshot();
       setImage(imageSrc);
       console.log(imageSrc);
     },[webcamRef]);
-
-
     let navigate = useNavigate(); 
-    const goHome = () =>{ 
+  const goHome = () =>{ 
       let path = '/'; 
       navigate(path);
-    }
-    return(
+  }
+  return(
         <div>
           <div className="NavBar">
             <ul>
@@ -68,23 +86,17 @@ function cameraPage(){
                 audio={false}
                 screenshotFormat="image/jpeg"
             />
-          <div className="submitButtonEncap">
-            <button  className = "submit" onClick={capture}>Capture</button>
-          </div>
+          <button className = "submit" onClick={capture}>Capture</button>
           </>
           ) : (
-            <>
-            <img  src ={image} alt = "screenshot"/>
-            <button  className = "submit"  onClick={() => setImage(null)}>Retake</button>
-            </>
-            
-          )};
+          <>  
+            <img className = "webcam" src ={image} alt = "screenshot"/>
+            <button className= "submit" onClick = {sendimage}>Send to Model</button>
+            <button className="submit" onClick={() => setImage(null)}>Retake</button>            
+          </>  
+          )}
    
           </div>
-          {/* <div className="submitButtonEncap">
-            <button  className = "submit" onClick={capture}>Capture</button>
-          </div> */}
-          
           <form onSubmit={handleUpload} className = "form">
                 <input id="file" type="file" name="file" multiple />
                 <button className = "submit"  type="submit" disabled={uploading}>Upload</button>
